72b03bd4445f64612346d977a3255d44f74f5328
Petro <pikhotapetro@gmail.com>
2018-07-01 22:42:15 +0300

    Initial commit
    First Version

---
+++ OpinionDB/Migrations/20180701180534_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace OpinionDB.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Opinion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    OpinionValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Opinion", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Opinion");
+        }
+    }
+}
---
+++ OpinionDB/Pages/ShowOpinion.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OpinionDB.Models;
+
+namespace OpinionDB.Pages
+{
+    public class ShowOpinionModel : PageModel
+    {
+        private readonly ApplicationContext _context;
+        public List<Opinion> opinions { get; set; }
+        public ShowOpinionModel(ApplicationContext db)
+        {
+            _context = db;
+        }
+        public void OnGet()
+        {
+            opinions = _context.Opinion.AsNoTracking().ToList();
+        }
+    }
+}
