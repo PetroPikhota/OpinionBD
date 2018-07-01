873082859b1205f54a4bafcbb4c7bb646880b124
Petro <pikhotapetro@gmail.com>
2018-07-01 22:42:15 +0300

    Initial commit
    Add "change" and "Delete"

---
+++ OpinionDB/Models/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpinionDB.Models
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<Opinion> Opinion { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ OpinionDB/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OpinionDB.Models;
+
+namespace OpinionDB.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationContext _context;
+        [BindProperty]
+        public Opinion Opinion { get; set; }
+        public IndexModel(ApplicationContext db)
+        {
+            _context = db;
+        }
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Opinion.Add(Opinion);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
